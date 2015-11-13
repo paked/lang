626a0669c1b076a6e34cadaf6c514bd963140fe1
Harrison <harrison@theshoebridges.com>
2015-11-13 23:35:53 +1100

    Add basic ast

---
+++ ast_test.go
@@ -0,0 +1,31 @@
+package lang
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAST(t *testing.T) {
+	p := Program{
+		scope: &Scope{
+			make(map[string]string),
+		},
+	}
+
+	assign := &AssignmentStatement{
+		Name:  "potato",
+		Type:  "string",
+		Value: "raw",
+	}
+
+	p.statements = append(p.statements, assign)
+
+	err := p.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if str := p.scope.Get("potato"); str != "raw" {
+		fmt.Println("Did not get correct value. Got", str, "expected", "raw")
+	}
+}
